99efc83a8d91251e40a03621c9387295e80473bc
Rowan S-L <rowan.with.cats@gmail.com>
2021-12-02 17:47:14 -0500

    whoops

--- code/errors/core_errors.py
+++ code/errors/core_errors.py
@@ -1,17 +1,21 @@
 import sys
 
+
 class ComputerError(Exception):
     """
     Base class for all Computer related errors
     """
 
+
 class UnstoppableComputerException(ComputerError):
     """
     Unkillable, Untouchable, Unstoppable, you know it, you love it, sys.exit()
     """
+
     def __init__(self, *args: object, **kwdargs: object) -> None:
         super().__init__(*args)
         sys.exit(1)
+
 
 class SegmentionFault(UnstoppableComputerException):
     """
--- code/main.py
+++ code/main.py
@@ -1,10 +1,12 @@
 from errors.core_errors import *
 from representation_types import *
 
+
 class Memory:
     """
     errr... a class to represent memory. provides usefull functions to deal with the memory.
     """
+
     def __init__(self, mem: list) -> None:
         self.mem = []
         for idx, val in enumerate(mem):
@@ -15,14 +17,14 @@ class Memory:
         Increases the memory's size by ammount, initializing it to Null
         """
         assert ammount > 0
-        self.mem += [Null]*ammount
+        self.mem += [Null] * ammount
 
     def enlarge_to(self, addr):
         """
         extends the memorys size so that the max adress is addr
         """
         current_max = self.size()
-        self.enlarge(addr-current_max)
+        self.enlarge(addr - current_max)
 
     def shrink_wrap(self):
         """
@@ -32,17 +34,17 @@ class Memory:
         removed = 0
         while True:
             if len(self.mem) == 0:
-                #nothing can be removed
+                # nothing can be removed
                 return removed
 
             if self.mem[-1] == Null:
-                #remove the null
+                # remove the null
                 del self.mem[-1]
-                #count it
+                # count it
                 removed += 1
 
             else:
-                #not a null value, return
+                # not a null value, return
                 return removed
 
     def reset(self):
@@ -57,11 +59,13 @@ class Memory:
         If adr is larger than the maximum address in memory, raises SegmentationFault
         """
         if adr > self.size():
-            #& oopsie whoopsise poopsise you messed up
-            raise SegmentionFault(f"Cannot set {val} at {adr}, as {adr} is larger than the maximun adress ({self.size()})!!")
+            # & oopsie whoopsise poopsise you messed up
+            raise SegmentionFault(
+                f"Cannot set {val} at {adr}, as {adr} is larger than the maximun adress ({self.size()})!!"
+            )
 
         elif adr < 0:
-            #& what did you expect
+            # & what did you expect
             raise ValueError("adr cannot be less than 0!")
 
         else:
@@ -73,11 +77,13 @@ class Memory:
         If adr is not in mem, raises SegmentationFault
         """
         if adr > self.size():
-            #& oopsie whoopsise poopsise you messed up
-            raise SegmentionFault(f"Cannot read value from {adr}, as {adr} is larger than the maximun adress ({self.size()})!!")
+            # & oopsie whoopsise poopsise you messed up
+            raise SegmentionFault(
+                f"Cannot read value from {adr}, as {adr} is larger than the maximun adress ({self.size()})!!"
+            )
 
         elif adr < 0:
-            #& what did you expect
+            # & what did you expect
             raise ValueError("adr cannot be less than 0!")
 
         else:
@@ -87,7 +93,7 @@ class Memory:
         """
         returns largest address that can be written to in memory
         """
-        return len(self.mem)-1
+        return len(self.mem) - 1
 
 
 class Computer:
